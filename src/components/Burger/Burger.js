8b47556b908c5d2f15e53be3d1d1368bd3519ac3
marianalm123 <marianaleitemelo4@gmail.com>
2020-07-14 20:01:29 +0100

    adding and removing ingredients; displaying price; order button;

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,21 @@ import classes from './Burger.css';
 
 const burger = (props) => {
   // transform an object of key value pairs into an array of burguer ingredients
-  const transformedIngredients = Object.keys(props.ingredients)
-      .map(igKey => {
+  // reduce is an arrow function which allow us to transform an array into something else
+  let transformedIngredients = Object.keys(props.ingredients)
+      .map( igKey => {
           return [...Array(props.ingredients[igKey])].map((_,i) => {
               return <BurgerIngredient key={igKey + i} type={igKey} />;
-          });
-      });
+          } );
+      } )
+      .reduce((arr, el) => {
+        return arr.concat(el);
+      }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
       console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
