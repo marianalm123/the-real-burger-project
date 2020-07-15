672612a07d5f7b1037d29b660b4ffdf1e97db519
marianalm123 <marianaleitemelo4@gmail.com>
2020-07-15 23:12:55 +0100

    creating custom Button Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,7 +85,11 @@ class BurgerBuilder extends Component{
 
   purchaseCancelHandler = () => {
     this.setState({purchasing: false});
- }
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You continue!');
+  }
 
   render () {
     const disabledInfo = {
@@ -99,7 +103,11 @@ class BurgerBuilder extends Component{
     return (
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients}/>
+          <OrderSummary
+              ingredients={this.state.ingredients}
+              purchaseCancelled={this.purchaseCancelHandler}
+              purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
