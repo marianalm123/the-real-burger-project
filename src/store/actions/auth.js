09464e64a3df253e2ae7308e47269ecdca4c59a8
marianalm123 <marianaleitemelo4@gmail.com>
2021-03-21 19:41:57 +0000

    Adding Sign In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,13 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBG6Jx2HzJbGFPQfJilIjikFLNcZ9L-hJI', authData)
+
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBG6Jx2HzJbGFPQfJilIjikFLNcZ9L-hJI';
+        if (!isSignup) {
+          url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBG6Jx2HzJbGFPQfJilIjikFLNcZ9L-hJI'
+        }
+
+        axios.post(url, authData)
             .then(response => {
               console.log(response);
               dispatch(authSuccess(response.data));
