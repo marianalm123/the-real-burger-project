62b0f4266792b05f922ff7030ccdc33b34532ab4
marianalm123 <marianaleitemelo4@gmail.com>
2021-04-08 14:21:44 +0100

    fixing Redirect; SideDrawer close when link clicked; Burger width;

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -9,21 +9,21 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary';
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if (props.open) {
-      attachedClasses = [classes.SideDrawer, classes.Open];
+        attachedClasses = [classes.SideDrawer, classes.Open];
     }
 
     return (
-      <Aux>
-        <Backdrop show={props.open} clicked={props.closed}/>
-        <div className={attachedClasses.join(' ')}>
-          <div className={classes.Logo}>
-            <Logo />
-          </div>
-          <nav>
-            <NavigationItems isAuthenticated={props.isAuth}/>
-          </nav>
-        </div>
-      </Aux>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems isAuthenticated={props.isAuth}/>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
