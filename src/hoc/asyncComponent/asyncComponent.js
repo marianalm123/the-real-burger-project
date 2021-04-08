9b8ab2a81a01358a1427a667173aed1f9ffe6f8d
marianalm123 <marianaleitemelo4@gmail.com>
2021-04-08 16:37:10 +0100

    added Lazy Loading (load code when needed);

---
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+//Load the code we need to load and don't load any unnecessary code
+const asyncComponent = (importComponent) => {
+    return class extends Component {
+        state = {
+            component: null
+        }
+
+        componentDidMount () {
+            importComponent()
+                .then(cmp => {
+                    this.setState({component: cmp.default});
+                });
+        }
+
+        render () {
+            const C = this.state.component;
+
+            return C ? <C {...this.props} /> : null;
+        }
+    }
+}
+
+export default asyncComponent;
